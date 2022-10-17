42c59e538afd54aa24a95cedf0d7ae9379658b14
Yaoming Huang <yaoming.huang@ischool.com.tw>
2022-10-17 16:15:58 +0800

    使用 koa demo。

--- index.mjs
+++ index.mjs
@@ -1,15 +1,28 @@
-import http from 'node:http';
-import fetch from 'node-fetch'
+import Koa from 'koa';
+import fetch from 'node-fetch';
 
-var server = http.createServer(async function (req, rsp) {
-  
-  const grsp = await fetch('https://1admin.ischool.com.tw/dsa4/h.trialschool.tw')
-  const data = await grsp.text();
+const app = new Koa();
 
-  rsp.write(data);
+app.use(async ctx => {
+  const { dsns, code } = ctx.query;
 
-  rsp.end();
+  if(!dsns || !code) { 
+    ctx.body = `<h1>缺少參數: dsns、code</h1>`;
+    return;
+  }
+
+  // https://dsaproxy.1campus.net/dev.sh_d/identity/b2ddafd4-1825-474f-b390-8796bed4d790
+  const grsp = await fetch(`https://dsaproxy.1campus.net/${dsns}/identity/${code}`);
+  const userInfo = await grsp.json();
+
+  ctx.set({ 'Content-Type': 'text/html; charset=utf-8' });
+  ctx.body = `
+  <h1>帳號: ${userInfo.Account}</h1>
+  <h1>角色: ${userInfo.RoleType}</h1>
+  <h3>原始內容</h3>
+  <pre>${JSON.stringify(userInfo, undefined, '\t')}</pre>
+  `;
 });
 
-server.listen(3000);
- console.log('1Campus App web server at port 3000 is running..')
+app.listen(3000);
+console.log('1Campus App web server at port 3000 is running..')
